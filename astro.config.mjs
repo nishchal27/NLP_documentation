e975061c7d48450125721b7f2aa407c4b032236d
nishchal <singhnishchal27@gmail.com>
2024-07-04 15:05:00 +0530

    Page: added folder structure document

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,37 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      customCss: ["./src/styles/main.css"],
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Code Docs",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Code Documentation", link: "/code-docs/example/" },
+            { label: "Test", link: "/code-docs/test/" },
+            { label: "Folder Structure", link: "/code-docs/folder-structure/" },
+          ],
+        },
+        {
+          label: "User Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/user-guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
